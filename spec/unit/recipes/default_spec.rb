71ac972d2b735a1ed13df750a6ab59e406cfadb6
Moustapha Akanmu <makanmu@spartaglobal.com>
2019-10-31 09:35:15 +0000

    initial commit

--- spec/unit/recipes/default_spec.rb
+++ spec/unit/recipes/default_spec.rb
@@ -10,20 +10,27 @@ describe 'nginx::default' do
   context 'When all attributes are default, on Ubuntu 18.04' do
     # for a complete list of available platforms and versions see:
     # https://github.com/chefspec/fauxhai/blob/master/PLATFORMS.md
-    platform 'ubuntu', '18.04'
+    platform 'ubuntu', '16.04'
 
     it 'converges successfully' do
       expect { chef_run }.to_not raise_error
     end
   end
-
-  context 'When all attributes are default, on CentOS 7' do
-    # for a complete list of available platforms and versions see:
-    # https://github.com/chefspec/fauxhai/blob/master/PLATFORMS.md
-    platform 'centos', '7'
-
-    it 'converges successfully' do
-      expect { chef_run }.to_not raise_error
-    end
+  it 'should enable nginx' do
+    expect(chef_run).to enable_service 'nginx'
   end
+
+  it 'should start the nginx service' do
+    expect(chef_run).to start_service 'nginx'
+  end
+
+  it 'should create proxy.conf template in etc/nginx/sites-available' do
+    expect(chef_run).to create_template('/etc/nginx/sites-available/proxy.conf').with_variables(proxy_port:3000)
+  end
+
+  it 'should delete the symlink from the default config in sites-enabled' do
+    expect(chef_run).to delete_link('/etc/nginx/sites-enabled/default')
+  end
+
+
 end
